af0c50941e61d07a46f8cdbf16ee74753aa09a4c
Kostyantyn Sharovarsky <kostya9@gmail.com>
2019-01-27 00:03:14 +0200

    Project structure

---
+++ src/KPI.RedditMonitor.Collector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KPI.RedditMonitor.Collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
