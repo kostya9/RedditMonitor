b0562ba4a0a2789496e37cfa8c0058b5f09b2437
Kostyantyn Sharovarsky <kostya9@gmail.com>
2019-06-08 17:15:57 +0300

    Add benchmarks

---
+++ src/KPI.RedditMonitor.Benchmarks/BegeBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using KPI.RedditMonitor.Application.Similarity;
+using System;
+using System.IO;
+
+namespace KPI.RedditMonitor.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ImageExtractionBenchmark
+    {
+        [Params("800x1086-101KB.jpg", "1125x1500-74KB.jpg", "2060x1920-765KB.jpg", "4032x3024-2681KB.jpg")]
+        public string Image { get; set; }
+
+        private Stream _file;
+
+        [IterationSetup]
+        public void ReadStuff()
+        {
+            _file = File.OpenRead($"./img/{Image}");
+        }
+
+        [IterationCleanup]
+        public void DisposeStuff()
+        {
+            _file.Dispose();
+        }
+
+        [Benchmark]
+        public void ExtractFeatures()
+        {
+            ImageFeatureFactory.Create(_file);
+        }
+    }
+}
---
+++ src/KPI.RedditMonitor.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace KPI.RedditMonitor.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ImageExtractionBenchmark>();
+        }
+    }
+}
